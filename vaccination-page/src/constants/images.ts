7b5f4c8927889464858cac72fadc0ede4eba16a6
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-08 16:47:41 +0700

    feat: create images.ts

---
+++ vaccination-page/src/constants/images.ts
@@ -0,0 +1,7 @@
+export const HERO_BANNER = {
+  default: 'assets/images/png/hero.png',
+  mobile: 'assets/images/webp/hero-mobile.webp',
+  mobile2x: 'assets/images/webp/hero-mobile-2x.webp',
+  tablet: 'assets/images/webp/hero-tablet.webp',
+  desktop: 'assets/images/webp/hero-desktop.webp',
+};
