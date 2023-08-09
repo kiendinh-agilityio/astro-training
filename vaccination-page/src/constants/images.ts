e09db183c1dee82ef84c8e24d1ee9485a8cbfa6d
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-09 10:25:01 +0700

    feat: create constants company for images.ts

--- vaccination-page/src/constants/images.ts
+++ vaccination-page/src/constants/images.ts
@@ -5,3 +5,36 @@ export const HERO_BANNER = {
   tablet: 'assets/images/webp/hero-tablet.webp',
   desktop: 'assets/images/webp/hero-desktop.webp',
 };
+
+// Company Section
+export const COMPANY_MODERNA = {
+  default: 'assets/images/png/moderna.png',
+  mobile: 'assets/images/webp/moderna.webp',
+  mobile2x: 'assets/images/webp/moderna-mobile-2x.webp',
+  tablet: 'assets/images/webp/moderna-tablet.webp',
+  desktop: 'assets/images/webp/moderna-desktop.webp',
+};
+
+export const COMPANY_PFIZER = {
+  default: 'assets/images/png/pfizer.png',
+  mobile: 'assets/images/webp/pfizer.webp',
+  mobile2x: 'assets/images/webp/pfizer-mobile-2x.webp',
+  tablet: 'assets/images/webp/pfizer-tablet.webp',
+  desktop: 'assets/images/webp/pfizer-desktop.webp',
+};
+
+export const COMPANY_ASTRAZENECA = {
+  default: 'assets/images/png/astra.png',
+  mobile: 'assets/images/webp/astra.webp',
+  mobile2x: 'assets/images/webp/astra-mobile-2x.webp',
+  tablet: 'assets/images/webp/astra-tablet.webp',
+  desktop: 'assets/images/webp/astra-desktop.webp',
+};
+
+export const COMPANY_SINOVAC = {
+  default: 'assets/images/png/sinovac.png',
+  mobile: 'assets/images/webp/sinovac.webp',
+  mobile2x: 'assets/images/webp/sinovac-mobile-2x.webp',
+  tablet: 'assets/images/webp/sinovac-tablet.webp',
+  desktop: 'assets/images/webp/sinovac-desktop.webp',
+};
