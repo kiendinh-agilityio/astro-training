d23317caff87fdfdb40f2601a528f884efd20c02
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-18 16:59:05 +0700

    feat: update uppercase for constants file

--- vaccination-page/src/constants/images.ts
+++ vaccination-page/src/constants/images.ts
@@ -1,49 +1,49 @@
 export const HERO_BANNER = {
-  default: 'assets/images/png/hero.png',
-  mobile: 'assets/images/webp/hero-mobile.webp',
-  mobile2x: 'assets/images/webp/hero-mobile-2x.webp',
-  tablet: 'assets/images/webp/hero-tablet.webp',
-  desktop: 'assets/images/webp/hero-desktop.webp',
+  DEFAULT: 'assets/images/png/hero.png',
+  MOBILE: 'assets/images/webp/hero-mobile.webp',
+  MOBILE2X: 'assets/images/webp/hero-mobile-2x.webp',
+  TABLET: 'assets/images/webp/hero-tablet.webp',
+  DESKTOP: 'assets/images/webp/hero-desktop.webp',
 };
 
 // Company Section
 export const COMPANY_MODERNA = {
-  default: 'assets/images/png/moderna.png',
-  mobile: 'assets/images/webp/moderna.webp',
-  mobile2x: 'assets/images/webp/moderna.webp',
-  tablet: 'assets/images/webp/moderna-tablet.webp',
-  desktop: 'assets/images/webp/moderna-desktop.webp',
+  DEFAULT: 'assets/images/png/moderna.png',
+  MOBILE: 'assets/images/webp/moderna.webp',
+  MOBILE2X: 'assets/images/webp/moderna.webp',
+  TABLET: 'assets/images/webp/moderna-tablet.webp',
+  DESKTOP: 'assets/images/webp/moderna-desktop.webp',
 };
 
 export const COMPANY_PFIZER = {
-  default: 'assets/images/png/pfizer.png',
-  mobile: 'assets/images/webp/pfizer.webp',
-  mobile2x: 'assets/images/webp/pfizer-mobile-2x.webp',
-  tablet: 'assets/images/webp/pfizer-tablet.webp',
-  desktop: 'assets/images/webp/pfizer-desktop.webp',
+  DEFAULT: 'assets/images/png/pfizer.png',
+  MOBILE: 'assets/images/webp/pfizer.webp',
+  MOBILE2X: 'assets/images/webp/pfizer-mobile-2x.webp',
+  TABLET: 'assets/images/webp/pfizer-tablet.webp',
+  DESKTOP: 'assets/images/webp/pfizer-desktop.webp',
 };
 
 export const COMPANY_ASTRAZENECA = {
-  default: 'assets/images/png/astra.png',
-  mobile: 'assets/images/webp/astra.webp',
-  mobile2x: 'assets/images/webp/astra-mobile-2x.webp',
-  tablet: 'assets/images/webp/astra-tablet.webp',
-  desktop: 'assets/images/webp/astra-desktop.webp',
+  DEFAULT: 'assets/images/png/astra.png',
+  MOBILE: 'assets/images/webp/astra.webp',
+  MOBILE2X: 'assets/images/webp/astra-mobile-2x.webp',
+  TABLET: 'assets/images/webp/astra-tablet.webp',
+  DESKTOP: 'assets/images/webp/astra-desktop.webp',
 };
 
 export const COMPANY_SINOVAC = {
-  default: 'assets/images/png/sinovac.png',
-  mobile: 'assets/images/webp/sinovac.webp',
-  mobile2x: 'assets/images/webp/sinovac-mobile-2x.webp',
-  tablet: 'assets/images/webp/sinovac-tablet.webp',
-  desktop: 'assets/images/webp/sinovac-desktop.webp',
+  DEFAULT: 'assets/images/png/sinovac.png',
+  MOBILE: 'assets/images/webp/sinovac.webp',
+  MOBILE2X: 'assets/images/webp/sinovac-mobile-2x.webp',
+  TABLET: 'assets/images/webp/sinovac-tablet.webp',
+  DESKTOP: 'assets/images/webp/sinovac-desktop.webp',
 };
 
 // Vaccine Registration section
 export const VACCINE_REGISTRATION = {
-  default: 'assets/images/png/vaccine-registration.png',
-  mobile: 'assets/images/webp/vaccine-registration.webp',
-  mobile2x: 'assets/images/webp/vaccine-registration-mobile-2x.webp',
-  tablet: 'assets/images/webp/vaccine-registration-tablet.webp',
-  desktop: 'assets/images/webp/vaccine-registration-desktop.webp',
+  DEFAULT: 'assets/images/png/vaccine-registration.png',
+  MOBILE: 'assets/images/webp/vaccine-registration.webp',
+  MOBILE2X: 'assets/images/webp/vaccine-registration-mobile-2x.webp',
+  TABLET: 'assets/images/webp/vaccine-registration-tablet.webp',
+  DESKTOP: 'assets/images/webp/vaccine-registration-desktop.webp',
 };
