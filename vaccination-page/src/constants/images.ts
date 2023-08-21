c4d005e18862c2abea717d5072e6fe147a878e3d
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-21 14:17:19 +0700

    feat: update file images.ts

--- vaccination-page/src/constants/images.ts
+++ vaccination-page/src/constants/images.ts
@@ -1,43 +1,60 @@
-export const HERO_BANNER = {
-  DEFAULT: 'assets/images/png/hero.png',
-  MOBILE: 'assets/images/webp/hero-mobile.webp',
-  MOBILE2X: 'assets/images/webp/hero-mobile-2x.webp',
-  TABLET: 'assets/images/webp/hero-tablet.webp',
-  DESKTOP: 'assets/images/webp/hero-desktop.webp',
+// Logo
+export const LOGO = {
+  DEFAULT: 'assets/images/svg/logo-default.svg',
+  MOBILE: 'assets/images/svg/logo-mobile.svg',
+  MOBILE2X: 'assets/images/svg/logo-mobile.svg',
+  TABLET: 'assets/images/svg/logo-tablet.svg',
+  DESKTOP: 'assets/images/svg/logo-desktop.svg',
 };
+
+// Hero Section
+export const HERO_IMAGES = {
+  HERO_BANNER: {
+    DEFAULT: 'assets/images/png/hero.png',
+    MOBILE: 'assets/images/webp/hero-mobile.webp',
+    MOBILE2X: 'assets/images/webp/hero-mobile-2x.webp',
+    TABLET: 'assets/images/webp/hero-tablet.webp',
+    DESKTOP: 'assets/images/webp/hero-desktop.webp',
+  },
+  CLOCK_ICON: {
+    DEFAULT: 'assets/images/svg/clock.svg',
+    MOBILE: 'assets/images/svg/clock.svg',
+    MOBILE2X: 'assets/images/svg/clock.svg',
+    TABLET: 'assets/images/svg/clock-tablet.svg',
+  },
+}
 
 // Company Section
-export const COMPANY_MODERNA = {
-  DEFAULT: 'assets/images/png/moderna.png',
-  MOBILE: 'assets/images/webp/moderna.webp',
-  MOBILE2X: 'assets/images/webp/moderna.webp',
-  TABLET: 'assets/images/webp/moderna-tablet.webp',
-  DESKTOP: 'assets/images/webp/moderna-desktop.webp',
-};
-
-export const COMPANY_PFIZER = {
-  DEFAULT: 'assets/images/png/pfizer.png',
-  MOBILE: 'assets/images/webp/pfizer.webp',
-  MOBILE2X: 'assets/images/webp/pfizer-mobile-2x.webp',
-  TABLET: 'assets/images/webp/pfizer-tablet.webp',
-  DESKTOP: 'assets/images/webp/pfizer-desktop.webp',
-};
-
-export const COMPANY_ASTRAZENECA = {
-  DEFAULT: 'assets/images/png/astra.png',
-  MOBILE: 'assets/images/webp/astra.webp',
-  MOBILE2X: 'assets/images/webp/astra-mobile-2x.webp',
-  TABLET: 'assets/images/webp/astra-tablet.webp',
-  DESKTOP: 'assets/images/webp/astra-desktop.webp',
-};
-
-export const COMPANY_SINOVAC = {
-  DEFAULT: 'assets/images/png/sinovac.png',
-  MOBILE: 'assets/images/webp/sinovac.webp',
-  MOBILE2X: 'assets/images/webp/sinovac-mobile-2x.webp',
-  TABLET: 'assets/images/webp/sinovac-tablet.webp',
-  DESKTOP: 'assets/images/webp/sinovac-desktop.webp',
-};
+export const COMPANY_IMAGES = {
+  COMPANY_MODERNA: {
+    DEFAULT: 'assets/images/png/moderna.png',
+    MOBILE: 'assets/images/webp/moderna.webp',
+    MOBILE2X: 'assets/images/webp/moderna.webp',
+    TABLET: 'assets/images/webp/moderna-tablet.webp',
+    DESKTOP: 'assets/images/webp/moderna-desktop.webp',
+  },
+  COMPANY_PFIZER: {
+    DEFAULT: 'assets/images/png/pfizer.png',
+    MOBILE: 'assets/images/webp/pfizer.webp',
+    MOBILE2X: 'assets/images/webp/pfizer-mobile-2x.webp',
+    TABLET: 'assets/images/webp/pfizer-tablet.webp',
+    DESKTOP: 'assets/images/webp/pfizer-desktop.webp',
+  },
+  COMPANY_ASTRAZENECA: {
+    DEFAULT: 'assets/images/png/astra.png',
+    MOBILE: 'assets/images/webp/astra.webp',
+    MOBILE2X: 'assets/images/webp/astra-mobile-2x.webp',
+    TABLET: 'assets/images/webp/astra-tablet.webp',
+    DESKTOP: 'assets/images/webp/astra-desktop.webp',
+  },
+  COMPANY_SINOVAC: {
+    DEFAULT: 'assets/images/png/sinovac.png',
+    MOBILE: 'assets/images/webp/sinovac.webp',
+    MOBILE2X: 'assets/images/webp/sinovac-mobile-2x.webp',
+    TABLET: 'assets/images/webp/sinovac-tablet.webp',
+    DESKTOP: 'assets/images/webp/sinovac-desktop.webp',
+  },
+}
 
 // Vaccine Registration section
 export const VACCINE_REGISTRATION = {
@@ -47,3 +64,91 @@ export const VACCINE_REGISTRATION = {
   TABLET: 'assets/images/webp/vaccine-registration-tablet.webp',
   DESKTOP: 'assets/images/webp/vaccine-registration-desktop.webp',
 };
+
+// Vaccinated section
+export const VACCINATED_IMAGES = {
+  SHIELD: {
+    DEFAULT: 'assets/images/svg/shield.svg',
+    MOBILE: 'assets/images/svg/shield.svg',
+    MOBILE2X: 'assets/images/svg/shield.svg',
+    TABLET: 'assets/images/svg/shield-tablet.svg',
+  },
+  ARROW_BLUE: {
+    DEFAULT: 'assets/images/svg/arrow-blue.svg',
+    MOBILE: 'assets/images/svg/arrow-blue.svg',
+    MOBILE2X: 'assets/images/svg/arrow-blue.svg',
+    TABLET: 'assets/images/svg/arrow-blue-tablet.svg',
+  },
+  USERS: {
+    DEFAULT: 'assets/images/svg/users.svg',
+    MOBILE: 'assets/images/svg/users.svg',
+    MOBILE2X: 'assets/images/svg/users.svg',
+    TABLET: 'assets/images/svg/users-tablet.svg',
+  },
+  THER_M0METER: {
+    DEFAULT: 'assets/images/svg/thermometer.svg',
+    MOBILE: 'assets/images/svg/thermometer.svg',
+    MOBILE2X: 'assets/images/svg/thermometer.svg',
+    TABLET: 'assets/images/svg/thermometer-tablet.svg',
+  },
+  ARROW: {
+    DEFAULT: 'assets/images/svg/arrow.svg',
+    MOBILE: 'assets/images/svg/arrow.svg',
+    MOBILE2X: 'assets/images/svg/arrow.svg',
+    TABLET: 'assets/images/svg/arrow-tablet.svg',
+  },
+};
+
+// Corona Image
+export const CORONA_IMAGE = {
+  DEFAULT: 'assets/images/svg/corona-virus.svg',
+  MOBILE: 'assets/images/svg/corona-virus.svg',
+  MOBILE2X: 'assets/images/svg/corona-virus.svg',
+  TABLET: 'assets/images/svg/corona-virus-tablet.svg',
+  DESKTOP: 'assets/images/svg/corona-virus-desktop.svg',
+};
+
+// Social Images
+export const SOCIAL_IMAGES = {
+  YOUTUBE: {
+    DEFAULT: 'asssets/images/svg/youtube.svg',
+    MOBILE: 'assets/images/svg/youtube.svg',
+    MOBILE2X: 'assets/images/svg/youtube.svg',
+    TABLET: 'assets/images/svg/youtube-tablet.svg',
+    DESKTOP: 'assets/images/svg/youtube-desktop.svg',
+  },
+  INSTAGRAM: {
+    DEFAULT: 'assets/images/svg/instagram.svg',
+    MOBILE: 'assets/images/svg/instagram.svg',
+    MOBILE2X: 'assets/images/svg/instagram.svg',
+    TABLET: 'assets/images/svg/instagram-tablet.svg',
+    DESKTOP: 'assets/images/svg/instagram-desktop.svg',
+  },
+  TWITTER: {
+    DEFAULT: 'assets/images/svg/twitter.svg',
+    MOBILE: 'assets/images/svg/twitter.svg',
+    MOBILE2X: 'assets/images/svg/twitter.svg',
+    TABLET: 'assets/images/svg/twitter-tablet.svg',
+    DESKTOP: 'assets/images/svg/twitter-desktop.svg',
+  },
+  FACEBOOK: {
+    DEFAULT: 'assets/images/svg/facebook.svg',
+    MOBILE: 'assets/images/svg/facebook.svg',
+    MOBILE2X: 'assets/images/svg/facebook.svg',
+    TABLET: 'assets/images/svg/facebook-tablet.svg',
+    DESKTOP: 'assets/images/svg/facebook-desktop.svg',
+  },
+}
+
+// Feedback section
+export const FEEDBACK_IMAGES = {
+  AVATAR_ONE: {
+    URL: 'assets/images/svg/oyindamola-maja.svg',
+  },
+  AVATAR_TWO: {
+    URL: 'assets/images/svg/okeowo-lekan.svg',
+  },
+  AVATAR_THREE: {
+    URL: 'assets/images/svg/kafaru-temitope.svg',
+  },
+}
