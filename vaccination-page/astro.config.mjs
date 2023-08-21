15c4dc20695333d3eb8fd65be9aeb4c1eb02d6c7
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-21 14:17:02 +0700

    feat: add astro-compress for page

--- vaccination-page/astro.config.mjs
+++ vaccination-page/astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel/serverless";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    compress({
+			css: true,
+			html: true,
+			js: true,
+			img: true,
+			svg: false,
+		}),
+  ],
   output: "server",
   adapter: vercel()
 });
