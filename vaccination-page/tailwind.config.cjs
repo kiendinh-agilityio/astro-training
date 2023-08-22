0b6ef20d7868328d279c6330304886347af28b79
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-22 19:00:37 +0700

    feat: implement switch theme color for page

--- vaccination-page/tailwind.config.cjs
+++ vaccination-page/tailwind.config.cjs
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
