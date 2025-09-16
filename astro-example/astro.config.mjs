8b7f6eaeccaebc882c3730feed333ed746818c4e
Kien Dinh <kien.dinh@asnet.com.vn>
2025-09-16 17:31:59 +0700

    feat: install package

--- astro-example/astro.config.mjs
+++ astro-example/astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   server: {
     port: 3000,
   },
+  output: "server",
 });
