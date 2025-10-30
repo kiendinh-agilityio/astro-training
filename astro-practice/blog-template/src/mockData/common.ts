a225b2f6bb9ba33162f5603f48898343d0273e6c
Kien Dinh <kien.dinh@asnet.com.vn>
2025-10-30 14:09:56 +0700

    feat: implement footer layout

---
+++ astro-practice/blog-template/src/mockData/common.ts
@@ -0,0 +1,15 @@
+import type { SocialItem } from '@/types';
+
+export const socialItems: SocialItem[] = [
+  { href: '#', ariaLabel: 'Instagram', icon: 'Instagram' },
+  { href: '#', ariaLabel: 'X (Twitter)', icon: 'Twitter' },
+  { href: '#', ariaLabel: 'LinkedIn', icon: 'Linkedin' },
+  { href: '#', ariaLabel: 'Facebook', icon: 'Facebook' },
+  { href: '#', ariaLabel: 'YouTube', icon: 'Youtube' },
+];
+
+export const legalLinks = [
+  { label: 'Privacy Policy', href: '/privacy-policy' },
+  { label: 'Terms of Service', href: '/terms-of-service' },
+  { label: 'Cookies Settings', href: '/cookies-settings' },
+];
