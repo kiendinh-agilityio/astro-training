43bfba99eed976088b17d1096e456a69cbc9346f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-10-30 09:34:43 +0700

    feat: implement ui for the header layout

--- astro-practice/blog-template/src/components/Icons/index.tsx
+++ astro-practice/blog-template/src/components/Icons/index.tsx
@@ -24,5 +24,30 @@ const Icons = {
       />
     </svg>
   ),
+
+  Burger: (props: IconProps) => (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g opacity="0.5">
+        <g clipPath="url(#clip0_3151_5418)">
+          <path
+            d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z"
+            fill="#212121"
+          />
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_3151_5418">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
 };
 export default Icons;
--- astro-practice/blog-template/src/components/index.ts
+++ astro-practice/blog-template/src/components/index.ts
@@ -1,6 +1,7 @@
-export { default as Link } from './Link/index.astro';
-export { default as Icons } from './Icons/index';
-export { default as Logo } from './Logo/index.astro';
-export { default as Heading } from './Heading/index.astro';
-export { default as Paragraph } from './Paragraph/index.astro';
 export { default as ButtonLink } from './ButtonLink/index.astro';
+export { default as Heading } from './Heading/index.astro';
+export { default as Icons } from './Icons/index';
+export { default as Link } from './Link/index.astro';
+export { default as Logo } from './Logo/index.astro';
+export { default as Navbar } from './Navbar/index';
+export { default as Paragraph } from './Paragraph/index.astro';
