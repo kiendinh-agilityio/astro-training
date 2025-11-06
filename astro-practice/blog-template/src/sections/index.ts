ed9367614bcb3b11e49faf519998c1b269ea4d06
Kien Dinh <kien.dinh@asnet.com.vn>
2025-11-06 17:21:57 +0700

    feat: split components

--- astro-practice/blog-template/src/sections/index.ts
+++ astro-practice/blog-template/src/sections/index.ts
@@ -1,3 +1,4 @@
 export { default as Blog } from './Blog/index.astro';
+export { default as BlogDetail } from './BlogDetail/index.astro';
 export { default as Hero } from './Hero/index.astro';
 export { default as Login } from './Login/index.astro';
